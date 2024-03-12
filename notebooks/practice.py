c6566ef83867e25ad11c244d8b0a36415864cb2b
Mohamed Illiyas <120028367+Illiyas124@users.noreply.github.com>
2024-03-12 05:34:10 +0000

    Workshop2

---
+++ notebooks/practice.py
@@ -0,0 +1,50 @@
+print('hello')
+# comment
+Name = "ILLIYAS"
+Name1 = "Illi"# Name , Name1 = "illiyas" , "data"
+print(Name)
+Customer_name = "Data"
+Watch_price  = 1590
+# cannot added =>>> print(Customer_name + Watch_price)
+
+#Strings
+word = 'Hii AI'
+word1 = "  My favourite persone age is 24  "
+#Cotes
+word2 = """This
+is 
+my
+para"""
+#word3 = 'Why didn't you come' ==>>> double cotes use this scanerios...
+print(word)
+print(word1)
+print(word2)
+#Array ===>>> String use like...(how to store the memory)
+word3 = "Hello!"
+print(word3[5]+word3[4])# space also get in a index...
+#slicing
+print(word3[1:4])
+print(word3[1:])
+print(word3[:2])
+print(word3[-4:-2])
+#length
+print(len(word2))
+#ignore the unwanted spacess strip()
+print(word1)
+print(word1.strip())
+#lower and upper keywords
+print(word1.upper().strip())
+#
+a = 'data'
+print(a)
+print(a.replace('d','D'))
+print(a.split('a'))
+#check if the word in or not
+b = "illiyasdata"
+print("data" in b)
+print("iii" in b)
+
+
+
+
+
